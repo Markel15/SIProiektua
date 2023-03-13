e1bbe2b314e4b4144fa823f0e28e07482ddd5356
IMujika03 <127774267+IMujika03@users.noreply.github.com>
2023-03-13 18:49:00 +0100

    Add files via upload

--- pokemon/pokemon/src/pokemon/JokalariKatalogoa.java
+++ pokemon/pokemon/src/pokemon/JokalariKatalogoa.java
@@ -1,5 +1,25 @@
 package klaseak;
 
-public class JokalariKatalogoa {
+import java.util.ArrayList;
 
+public class JokalariKatalogoa {
+	private ArrayList<Jokalari> JokalariZer;
+	private static JokalariKatalogoa nJK;
+	
+	private JokalariKatalogoa() {
+		this.JokalariZer=new ArrayList<Jokalari>();
+	}
+	public static synchronized JokalariKatalogoa getnJK() {
+		if(nJK==null) {
+			nJK= new JokalariKatalogoa();
+			
+		}
+		return (nJK);
+	}
+	public void addJok (Jokalari pJok) {
+		this.JokalariZer.add(pJok);
+	}
+	public void deleteJok (Jokalari pJok) {
+		this.JokalariZer.remove(pJok);
+	}
 }
