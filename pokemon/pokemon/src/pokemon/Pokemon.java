77e3cd04398cccec210c45015040fcf32e015a2d
IMujika03 <127774267+IMujika03@users.noreply.github.com>
2023-03-19 19:54:15 +0100

    Add files via upload

--- pokemon/pokemon/src/pokemon/Pokemon.java
+++ pokemon/pokemon/src/pokemon/Pokemon.java
@@ -3,23 +3,26 @@ package pokemon;
 import java.util.Random;
 
 public abstract class Pokemon {
-	private int erasoa = 11;
-	private int defentsa = 3;
-	private int bizia = 200;
-	private String mota;
-	
+	protected int erasoa = 11;
+	protected int defentsa = 3;
+	protected int bizia = 200;
+	protected String mota;
+	protected int biziMax;
+	//protected int euphoria;
+
 	public Pokemon() {
 		this.erasoa = this.erasoa + new Random().nextInt(7) + 1;
 		this.defentsa = this.defentsa + new Random().nextInt(4) + 1;
 		this.bizia = this.bizia + new Random().nextInt(20) + 1;
+		this.biziMax = bizia;
 	}
-	
+
 	public void inprimatu() {
 		System.out.println(this.erasoa);
 		System.out.println(this.defentsa);
 		System.out.println(this.bizia);
 	}
-	
+
 	public static void main(String[] args) {
 		PokemonFactory f = PokemonFactory.getPokemonFactory();
 		Pokemon p = f.sortuPokemon("Ur");
@@ -28,4 +31,18 @@ public abstract class Pokemon {
 	public void setMota(String pMota) {
 		this.mota=pMota;
 	}
+	public int getBizia() {
+		return(this.bizia);
+	}
+	public int lortuBiziKop() {
+		return((this.bizia/this.biziMax)*100);
+	}
+	public void biziaKendu(int kop) {
+		this.bizia=-kop;
+	}
+	public int getErasoa() {
+		return this.erasoa;
+	}
+	
+	
 }
