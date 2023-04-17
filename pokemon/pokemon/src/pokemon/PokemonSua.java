96b35ed83049fa82381d36c0347ced63453af287
ABenito03 <127773926+ABenito03@users.noreply.github.com>
2023-04-17 18:06:57 +0200

    Update PokemonSua.java

--- pokemon/pokemon/src/pokemon/PokemonSua.java
+++ pokemon/pokemon/src/pokemon/PokemonSua.java
@@ -8,7 +8,8 @@ public class PokemonSua extends Pokemon {
 
 	@Override
 	protected int biderkatzaileaLortu(String pMota) {
-		if(pMota.equals("Belarra"))return 2;
+		if(pMota.equals("Belarra") || pMota.equals("Zomorro"))return 2;
+		//else if(pMota.equals("Ura") || pMota.equals("Arroka") || pMota.equals("Sua") || pMota.equals("Dragoia"))return 0.5;
 		else return 1;
 	}
 }
