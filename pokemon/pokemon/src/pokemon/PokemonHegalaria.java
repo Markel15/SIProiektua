cab8b25e76f86c8c917381a0ff8e9396dc52ea4e
ABenito03 <127773926+ABenito03@users.noreply.github.com>
2023-04-17 18:03:11 +0200

    Update PokemonHegalaria.java

--- pokemon/pokemon/src/pokemon/PokemonHegalaria.java
+++ pokemon/pokemon/src/pokemon/PokemonHegalaria.java
@@ -8,7 +8,8 @@ public class PokemonHegalaria extends Pokemon {
 
 	@Override
 	protected int biderkatzaileaLortu(String pMota) {
-		if(pMota.equals(" "))return 2;
+		if(pMota.equals("Belarra") || pMota.equals("Zomorro") || pMota.equals("Borroka"))return 2;
+		//else if(pMota.equals("Elektrikoa") || pMota.equals("Arroka"))return 0.5;
 		else return 1;
 	}
 }
