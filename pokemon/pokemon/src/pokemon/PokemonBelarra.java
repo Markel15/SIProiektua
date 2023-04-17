d7a9f2ee07868d419c6c4dd8e24264acbf38d5ef
ABenito03 <127773926+ABenito03@users.noreply.github.com>
2023-04-17 18:11:50 +0200

    Update PokemonBelarra.java

--- pokemon/pokemon/src/pokemon/PokemonBelarra.java
+++ pokemon/pokemon/src/pokemon/PokemonBelarra.java
@@ -8,7 +8,7 @@ public class PokemonBelarra extends Pokemon {
 
 	@Override
 	protected int biderkatzaileaLortu(String pMota) {
-		if(pMota.equals("Elektrikoa") || pMota.equals("Ura") || pMota.equals("Arroka"))return 2;
+		if(pMota.equals("Elektrikoa") || pMota.equals("Ur") || pMota.equals("Arroka"))return 2;
 		//else if(pMota.equals("Zomorro") || pMota.equals("Belarra") || pMota.equals("Sua") || pMota.equals("Dragoia") || pMota.equals("Hegalaria"))return 0.5;
 		else return 1;
 	}
